5fac014d47f4fe54b6c48fb80dc1cbaa63312314
equaldigits <rui.nz@hotmail.com>
2018-02-16 15:51:24 +0000

    third commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,5 +24,9 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+
+        $completeTasks = DB::table('tasks')->where('completed = 1')->get();
+
+        $incompleteTasks = DB::table('tasks')->where('completed = 0')->get();
     }
 }
